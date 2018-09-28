ba45d01089d7bda0443e3fb930f07ef7d7a53bf3
LeonardoChen <lc@LeonardoChens-MacBook-Pro.local>
2018-09-28 16:45:08 +0800

    hihi

--- bin/dev_console.sh
+++ bin/dev_console.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # -config app.config -config app.last.config \
 
-erl -pa ./ebin/ -boot start_sasl -pa deps/*/ebin -config app.config +P 3000000 -name chen@pilot.skye.gem -mnesia dir mnesia -s lager
+erl  -env ERL_LIBS _build/default/deps -config app.config +P 3000000 -name chen@pilot.skye.gem -mnesia dir mnesia -s lager
 #erl -pa ./ebin/ -boot start_sasl -pa deps/*/ebin -config app.config +P 3000000 -name leon@pilot.skye.gem -mnesia dir '"/Users/lc/lab/mnesia"' -s lager
 
 
---
+++ include/windforce_application.hrl
@@ -0,0 +1,44 @@
+%%%-------------------------------------------------------------------
+%%% @author lc
+%%% @copyright (C) 2018, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 28. Sep 2018 15:32
+%%%-------------------------------------------------------------------
+-module(windforce_application).
+-author("lc").
+
+-define(CHILD(I, Type), {I, {I, start_link, []}, permanent, 5000, Type, [I]}).
+
+-define(GET_APP_ENV(Key, Default_Value), application:get_env(application:get_application(), Key, Default_Value)).
+
+-define(SUP_FLAGS_ONE_FOR_ONE, #{strategy => one_for_one, intensity => ?MAX_INTENSITY, period => ?MAX_PERIOD}).
+
+-define(SUP_FLAGS_SIMPLE_ONE_FOR_ONE, #{strategy => simple_one_for_one, intensity => ?MAX_INTENSITY, period => ?MAX_PERIOD}).
+
+-define(CHILD_WORKER(Module, Args), #{id => Module,
+                                      start =>{Module, start_link, Args},
+                                      restart => transient,
+                                      shutdown => 2000,
+                                      type => worker,
+                                      modules => [Module]}).
+-define(CHILD_TEMP_WORKER(Module, Args), #{id => Module,
+                                      start =>{Module, start_link, Args},
+                                      restart => temporary,
+                                      shutdown => 2000,
+                                      type => worker,
+                                      modules => [Module]}).
+
+-define(CHILD_SUPERVISOR(Module, Args), #{id => Module,
+                                          start =>{Module, start_link, Args},
+                                          restart => transient,
+                                          shutdown => infinity,
+                                          type => supervisor,
+                                          modules => [Module]}).
+
+-define(MAX_INTENSITY, 1000).
+
+-define(MAX_PERIOD, 3600).
+
+
